8d46b6645a67ebf7d5d96b7c9b752b6ee6ecffe3
Abdul Rahman <tugas.abdulrahman@gmail.com>
2022-07-20 21:04:53 +0700

    add body

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Abdul Rahman Blog</title>
+</head>
+
+<body>
+    <h1>Halaman About</h1>
+</body>
+
+</html>
